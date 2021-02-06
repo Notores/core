89e570505123fa2937ec8d147866e7a93642566d
Kevin Sijbers <kevin@3tdev.nl>
2021-02-06 15:30:16 +0100

    build(dist): new distribution files

--- dist/constants.js
+++ dist/constants.js
@@ -1,12 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.IS_POST_MIDDLEWARE = exports.IS_PRE_MIDDLEWARE = exports.MIDDLEWARE = exports.ROLES = exports.AUTH = exports.PRIVATE = exports.PATH_ROUTE = exports.MODULE_PATH = exports.DATA_KEY = exports.ROOT_ROUTE = void 0;
+exports.IS_POST_MIDDLEWARE = exports.IS_PRE_MIDDLEWARE = exports.MIDDLEWARE = exports.ROLES = exports.AUTH = exports.PRIVATE = exports.PATH_ROUTE = exports.IGNORE_DATA_KEY = exports.MODULE_PATH = exports.DATA_KEY = exports.ROOT_ROUTE = void 0;
 /**
  * MODULE CONSTANTS
  */
 exports.ROOT_ROUTE = 'ROOT_ROUTE';
 exports.DATA_KEY = 'DATA_KEY';
 exports.MODULE_PATH = 'MODULE_PATH';
+exports.IGNORE_DATA_KEY = 'IGNORE_DATA_KEY';
 /**
  * HTTP METHOD CONSTANTS
  */
